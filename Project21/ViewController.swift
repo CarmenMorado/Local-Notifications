5c0b454288c1ebf705aa02a2ae47bec74ea639aa
Carmen Morado <cmora171@fiu.edu>
2021-04-13 13:11:26 -0400

    The code in didReceive has been updated so that it shows different instances of UIAlertController depending on which action identifier was passed in.
    Also, a second UNNotificationAction has been added to the alarm category in the method registerCategories() with the title “Remind me later...”. It has been added as a new case in didReceive and it calls scheduleLocal() so that the same alert is shown in 24 hours.

--- Project21/ViewController.swift
+++ Project21/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         super.viewDidLoad()
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleTapped))
     }
     
     @objc func registerLocal() {
@@ -29,7 +29,7 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         }
     }
     
-    @objc func scheduleLocal() {
+    @objc func scheduleLocal(after timeInterval: TimeInterval) {
         registerCategories()
 
         let center = UNUserNotificationCenter.current()
@@ -45,8 +45,7 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         var dateComponents = DateComponents()
         dateComponents.hour = 10
         dateComponents.minute = 30
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-        //let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeInterval, repeats: false)
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
         center.add(request)
     }
@@ -56,7 +55,8 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         center.delegate = self
 
         let show = UNNotificationAction(identifier: "show", title: "Tell me more…", options: .foreground)
-        let category = UNNotificationCategory(identifier: "alarm", actions: [show], intentIdentifiers: [])
+        let remind = UNNotificationAction(identifier: "remind", title: "Remind me later...", options: .foreground)
+        let category = UNNotificationCategory(identifier: "alarm", actions: [show, remind], intentIdentifiers: [])
 
         center.setNotificationCategories([category])
     }
@@ -72,11 +72,20 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
             case UNNotificationDefaultActionIdentifier:
                 // the user swiped to unlock
                 print("Default identifier")
+                let alertController = UIAlertController(title: "Welcome to Project 21", message: nil, preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+                present(alertController, animated: true)
 
             case "show":
                 // the user tapped our "show more info…" button
                 print("Show more information…")
-
+                let alertController = UIAlertController(title: "More Info", message: "100 Days of Swift (Project 21) Technique project about local notifications, which allow the sending of reminders to the user's lock screen to show them information when the app isn't running.", preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+                present(alertController, animated: true)
+                
+            case "remind":
+                scheduleLocal(after: 86400)
+                
             default:
                 break
             }
@@ -84,6 +93,10 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
 
         // you must call the completion handler when you're done
         completionHandler()
+    }
+    
+    @objc func scheduleTapped() {
+        scheduleLocal(after: 5)
     }
 
 
